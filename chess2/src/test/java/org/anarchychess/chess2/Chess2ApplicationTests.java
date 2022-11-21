c21f489c98335968ef9657fd4772aee2cd6138fd
Morfe0 <101822150+Morfe0@users.noreply.github.com>
2022-11-21 18:44:42 +0100

    Add files via upload

---
+++ chess2/src/test/java/org/anarchychess/chess2/Chess2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.anarchychess.chess2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chess2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
